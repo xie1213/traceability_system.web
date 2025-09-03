0c78bf00df989674c188ee8c5d5d147016a94e58
青木 <3285318521@qq.com>
2025-09-03 14:05:45 +0800

    页面表头美化

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
-import { createApp } from "vue";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-import axios from "axios";
-import VXETable from "vxe-table";
-import "vxe-table/lib/style.css";
-import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import axios from 'axios';
+import VXETable from 'vxe-table';
+import 'vxe-table/lib/style.css';
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 
 // import router from "@/router"
-import "vant/lib/index.css";
-import App from "./App.vue";
+import 'vant/lib/index.css';
+import App from './App.vue';
 let host = window.location.host; // 主机
 let reg = /^localhost+/;
 if (reg.test(host)) {
-    // 若本地项目调试使用
-    console.log("本地");
-    // axios.defaults.baseURL = "http://172.31.12.201:5001";
-    axios.defaults.baseURL = "http://172.31.13.200:5000";
+  // 若本地项目调试使用
+  console.log('本地');
+  // axios.defaults.baseURL = "http://172.31.12.201:5001";
+  axios.defaults.baseURL = 'http://127.0.0.1:5000';
 }
 const app = createApp(App);
 app.use(ElementPlus, { locale: zhCn });
 app.use(VXETable);
 // app.use(router).mount('#app')
-app.mount("#app");
+app.mount('#app');
